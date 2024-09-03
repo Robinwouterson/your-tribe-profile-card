2632a654348c52a562acc22a5a4e87a2e6e13888
Robinwouterson <Robin_wouterson@yahoo.com>
2024-09-04 00:19:15 +0200

    Text en kleuren aangepast
    Ik heb de tekst gewijzigd, zoals mijn naam en overige zinnen. Ook heb ik de beker emoji's vervangen voor groene hartjes. Verder heb ik de kleuren aangepast in de CSS stylesheet. Ik heb gekozen voor een groene kleur in verschillende tinten.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+const expandButton = document.querySelector('header button') //Deze regel zoekt in het huidige HTML document naar de eerste knop die zich binnen een header element bevindt en slaat een verwijzing naar deze knop op in de constante variabele expandButton.
+expandButton.addEventListener('click', expand) //Deze regel voegt een event listener toe aan de expandButton. Wanneer de knop wordt aangeklikt, wordt de functie expand uitgevoerd. Oftewel, de visitekaartje word uitvergroot.
 
 function expand () {
   document.body.classList.toggle('expand')
-}
+} //Deze functie voegt of verwijdert de CSS klasse 'expand' toe aan het <body> element. Deze klasse wordt waarschijnlijk gebruikt om de stijl van het visitekaartje aan te passen wanneer de uitbreidingsknop (expand knop) wordt aangeklikt.
